e91019f81fc7e852926fe60ff282ccb9ba725fa4
Moslem Tayari <avapardazmota@gmail.com>
2024-11-04 15:51:18 +0330

    fix some minor issues

--- src/models/AdvanceMoney.php
+++ src/models/AdvanceMoney.php
@@ -56,58 +56,6 @@ class AdvanceMoney extends AdvanceMoneyBase implements SendAutoCommentInterface
     }
 
     /**
-     * @return array
-     */
-    public function getUserMail(): array
-    {
-        if (in_array($this->getScenario(), [self::SCENARIO_CONFIRM, self::SCENARIO_REJECT])) {
-            return [$this->user_id];
-        }
-        return [];
-    }
-
-    /**
-     * @return string
-     */
-    public function getLinkMail(): string
-    {
-        if (in_array($this->getScenario(), [self::SCENARIO_CONFIRM, self::SCENARIO_REJECT])) {
-            return '';
-        }
-        return Yii::$app->urlManager->createAbsoluteUrl([Module::createUrl('employee-advance-money/index'), 'id' => $this->id]);
-    }
-
-    /**
-     * @return string
-     */
-    public function getContentMail(): string
-    {
-        $content = '';
-        if ($this->getScenario() == self::SCENARIO_CREATE) {
-            $content = Html::tag('p', "یک درخواست مساعده در سیستم ثبت شده است.");
-            $content .= Html::tag('p', 'این درخواست توسط "' . $this->user->fullName . '" در سیستم ثبت شد.');
-            $content .= Html::tag('p', 'مبلغ درخواست : "' . number_format((float)$this->amount) . '"');
-        }
-        if ($this->getScenario() == self::SCENARIO_REJECT) {
-            $content = Html::tag('p', "متاسفانه درخواست مساعده شما مورد تایید قرار نگرفت.");
-            $content .= Html::tag('p', 'این درخواست توسط "' . $this->update->fullName . '" رد شد.');
-            $content .= Html::tag('p', 'توضیحات رد درخواست : "' . $this->reject_comment . '"');
-            $content .= Html::tag('p', 'مبلغ درخواست : "' . number_format((float)$this->amount) . '"');
-        }
-        if ($this->getScenario() == self::SCENARIO_CONFIRM) {
-            $content = Html::tag('p', "درخواست مساعده شما مورد تایید قرار گرفت.");
-            $content .= Html::tag('p', 'این درخواست توسط "' . $this->update->fullName . '" تایید شد.');
-            $content .= Html::tag('p', 'مبلغ درخواست : "' . number_format((float)$this->amount) . '"');
-        }
-        return $content;
-    }
-
-    public function autoCommentCondition(): bool
-    {
-        return true;
-    }
-
-    /**
      * @return bool
      */
     public function canTransferToMultiPay(): bool
